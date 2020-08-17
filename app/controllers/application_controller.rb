66e42d0739c56842f0f111414e7af991f327ca08
Fifty-typhoon <68638938+Fifty-typhoon@users.noreply.github.com>
2020-08-17 15:39:57 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |id, password|
+      id == 'authorize' && password == '0987'
+        id == ENV["BASIC_AUTH_ID"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
