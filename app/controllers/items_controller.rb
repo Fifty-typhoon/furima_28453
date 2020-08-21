987fea9d5d8fef8b10bed10b29d59c4686e12904
Fifty-typhoon <68638938+Fifty-typhoon@users.noreply.github.com>
2020-08-21 19:20:44 +0900

    ログイン、ログアウトの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,15 @@
 class ItemsController < ApplicationController
 
+  before_action :move_to_sign_in, except: [:index, :show]
   def index
   end
   
+  def show
+  end
+
+  def move_to_sign_in
+   unless user_signed_in?
+     redirect to user_session_path
+   end
+  end
 end
