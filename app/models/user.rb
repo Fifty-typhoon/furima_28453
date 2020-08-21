e0b7913b1fcd1f732923038cacdf075fdab175d8
Fifty-typhoon <68638938+Fifty-typhoon@users.noreply.github.com>
2020-08-21 14:23:25 +0900

    サインアップ機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true   #uniqueness: true
   validates :encrypted_password, presence: true
   validates :nickname, presence: true
-  validates :first_name, presemce: true
+  validates :first_name, presence: true
   validates :last_name, presence: true
   validates :kana_first_name, presence: true
   validates :kana_last_name, presence: true
   validates :birthday, presence: true
-  #EMAIL_REGEX = /@.+/.freeze
-  #validates_format_of :email, with:EMAIL_REGEX
-  #PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  #validates_format_of :password, with: PASSWORD_REGEX
+
+  EMAIL_REGEX = /@.+/.freeze
+  validates_format_of :email, with:EMAIL_REGEX
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  FIRST_NAME = /[ぁ-んァ-ン一-龥]/.freeze
+  validates_format_of :first_name, with: FIRST_NAME
+  LAST_NAME = /[ぁ-んァ-ン一-龥]/.freeze
+  validates_format_of :last_name, with: LAST_NAME
+  KANA_FIRST_NAME = /[ァ-ン]/.freeze
+  validates_format_of :kana_first_name, with: KANA_FIRST_NAME
+  KANA_LAST_NAME = /[ァ-ン]/.freeze
+  validates_format_of :kana_last_name, with: KANA_LAST_NAME
+
 end
