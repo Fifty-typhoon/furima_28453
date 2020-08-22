a719dc494e483859805d2e5e007ebbd01433bf02
Fifty-typhoon <68638938+Fifty-typhoon@users.noreply.github.com>
2020-08-22 15:43:29 +0900

    factory_bot_rails、fakerの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  describe
-    it ""
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+      #user = User.new(nickname: "", encrypted_email: "kkk@gmail.com", password: "0912oiqw", first_name: "剛田", last_name: "武", kana_first_name: "ゴウダ", kana_last_name: "タケシ", birthday: "2000/1/1")
+      #user.valid?
     end
-    it ""
+    it "encrypted_emailが空だと登録できない" do
+    end
+    it "passwordが空だと登録できない" do
+    end
+    it "password_confirmationが空だと登録できない" do
+    end
+    it "first_nameが空だと登録できない" do
+    end
+    it "last_nameが空だと登録できない" do
+    end
+    it "kana_first_nameが空だと登録できない" do
+    end
+    it "kana_last_nameが空だと登録できない" do
+    end
+    it "birthdayが空だと登録できない" do
     end
   end
 end
