df8eed6a5406543d26c4a8dc784d42f636ec6625
Fifty-typhoon <68638938+Fifty-typhoon@users.noreply.github.com>
2020-09-14 17:29:54 +0900

    SNSアカウントでのログイン機能実装
    フロントのスタイリングは未だ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   with_options presence: true do
     validates :email   # uniqueness: true
@@ -17,6 +17,7 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :purchases
+  has_many :sns_credentials
 
   EMAIL_REGEX = /@.+/.freeze
   validates_format_of :email, with: EMAIL_REGEX
@@ -28,4 +29,20 @@ class User < ApplicationRecord
   KANA_NAME = /[ァ-ン]/.freeze
   validates_format_of :kana_first_name, with: KANA_NAME
   validates_format_of :kana_last_name, with: KANA_NAME
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    {user: user, sns: sns}
+  end
 end
