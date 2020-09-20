73942cf436e6a126b85c05e71fc1941607614845
Fifty-typhoon <68638938+Fifty-typhoon@users.noreply.github.com>
2020-09-21 03:09:46 +0900

    エラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,13 +37,13 @@ class User < ApplicationRecord
     # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = User.where(email: auth.info.email).first_or_initialize(
       nickname: auth.info.name,
-      email: auth.info.email
+        email: auth.info.email
     )
-    # userが登録済みであるか判断
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
     if user.persisted?
       sns.user = user
       sns.save
     end
-    {user: user, sns: sns}
+    { user: user, sns: sns }
   end
 end
